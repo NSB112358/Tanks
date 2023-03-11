a11c44a0c3d5640f14ccb35b464a11ddc9c28cc4
NSB112358 <127120165+NSB112358@users.noreply.github.com>
2023-03-11 16:51:27 +0300

    upload

--- project/main.py
+++ project/main.py
@@ -1,9 +1,12 @@
 import pygame
 
+from Tanks import Tank
+from Controls import driving
+
 pygame.init()
 
 # Размер окна
-size = (700, 500)
+size = (700, 600)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Моя игра")
@@ -14,6 +17,9 @@ done = False
 # Используется для управления частотой обновления экрана
 clock = pygame.time.Clock()
 
+player_tank = Tank(100, 100)
+enemy_tank = Tank(500, 500)
+
 # Основной цикл программы
 while not done:
     # --- Главный цикл обработки событий ---
@@ -22,9 +28,16 @@ while not done:
             done = True
 
     # --- Логика игры здесь ---
+    driving(player_tank)
+    
 
     # --- Рисование здесь ---
+    screen.fill((0, 0, 0))
 
+    screen.blit(player_tank.image, player_tank.rect)
+    screen.blit(enemy_tank.image, enemy_tank.rect)
+
+    pygame.display.flip()
     # --- Конец рисования ---
 
     # Ограничение до 60 кадров в секунду
